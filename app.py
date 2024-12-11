fcae681b293cacd27faf4ef1ecc78e90981a7b6c
Álvaro Rösler <63856707+alvarorosler@users.noreply.github.com>
2024-12-11 16:15:20 -0300

    Update Geral - Correções de Bugs app.py

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ import numpy as np
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 from statsmodels.tsa.seasonal import seasonal_decompose
 import plotly.graph_objects as go
-import matplotlib.pyplot as plt
+import locale
 
-# Criação do DataFrame 2G e variáveis exógenas de apoio do 1G e JE
+# Configuração do locale para português
+locale.setlocale(locale.LC_TIME, 'pt_BR.UTF-8')  # Configuração de meses em português
+
+# Criação do DataFrame com dados simulados
 data = {
     "Data": [
         "2021-06-01", "2021-07-01", "2021-08-01", "2021-09-01", "2021-10-01", "2021-11-01", "2021-12-01",
@@ -31,35 +34,23 @@ data = {
         17753, 17365, 18103, 16760, 19821, 19348, 10078, 10720, 17376, 24818, 19650, 19660, 19072, 21981, 21494,
         19221, 16608, 20308, 11906, 10146, 17042, 22458, 17386, 22301, 22078, 21185, 23906, 20051, 19444, 22048,
         14796, 10956, 19739, 19046, 23512, 20710, 20440, 23236, 21525, 22294, 22175, 22262
-    ],
-    "Sentenças_JE": [
-        18863, 19856, 22137, 22173, 19551, 21065, 14214, 11987, 18521, 22727, 20240, 24328, 24043, 23402, 26644,
-        25914, 22816, 24518, 15195, 14465, 24928, 30768, 22134, 29769, 30610, 26651, 29341, 27624, 29727, 28087,
-        16360, 15832, 23838, 25241, 27921, 27249, 28474, 32874, 29293, 30977, 31149, 26814
-    ],
-    "Sentenças_1G": [
-        53502, 58446, 61225, 57419, 54986, 62962, 63021, 47877, 68128, 79135, 64425, 69594, 68537, 71062, 73181,
-        69477, 67641, 77003, 75801, 55216, 65747, 79264, 64102, 78936, 73324, 73785, 79024, 75082, 74900, 74220,
-        74222, 72113, 72503, 80012, 78849, 73568, 73559, 81543, 79823, 77201, 95247, 76834
     ]
 }
 
+# Criação do DataFrame
 df = pd.DataFrame(data)
 
-# Converter a coluna 'Data' para o tipo datetime
+# Conversão e indexação por data
 df['Data'] = pd.to_datetime(df['Data'])
-
-# Definir a coluna 'Data' como índice
 df.set_index('Data', inplace=True)
 
 ################
 
 # Função para criar e ajustar o modelo SARIMAX
 def sarimax_model(endog, exog=None, steps=12):
-    # SARIMAX(1,1,1)(1,1,1)[12]
     model = SARIMAX(endog, exog=exog, order=(1, 1, 1), seasonal_order=(1, 1, 1, 12))
     model_fit = model.fit(disp=False)
-    forecast = model_fit.get_forecast(steps=steps, exog=exog[-steps:])
+    forecast = model_fit.get_forecast(steps=steps, exog=exog.iloc[-steps:])
     forecast_index = pd.date_range(endog.index[-1], periods=steps+1, freq='M')[1:]
     forecast_values = forecast.predicted_mean
     return model_fit, forecast_values, forecast_index
@@ -67,9 +58,12 @@ def sarimax_model(endog, exog=None, steps=12):
 # Função para decomposição de séries temporais
 def decompose_series(series):
     decomposition = seasonal_decompose(series, model='additive', period=12)
+    decomposition.trend = decomposition.trend.interpolate(method='linear')
+    decomposition.trend = decomposition.trend.reindex(series.index)
     return decomposition
 
-# Função para criar gráficos de série temporal e decomposição
+# Funções para gráficos
+
 def plot_series(actual, predicted, index, title):
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=actual.index, y=actual.values, mode='lines', name='Observado'))
@@ -83,36 +77,9 @@ def plot_decomposition(decomposition, title):
     fig.update_layout(title=title, xaxis_title='Data', yaxis_title='Tendência')
     st.plotly_chart(fig)
 
-def plot_seasonality(decomposition, title):
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=decomposition.seasonal.index, y=decomposition.seasonal, mode='lines', name='Sazonalidade'))
-    fig.update_layout(title=title, xaxis_title='Data', yaxis_title='Sazonalidade')
-    st.plotly_chart(fig)
-
-# Função para plotar o índice de sazonalidade mensal em relação ao desvio percentual da média anual
-def plot_seasonal_index(series, title):
-    # Cálculo da média mensal
-    monthly_avg = series.groupby(series.index.month).mean()
-
-    # Cálculo da média anual
-    annual_avg = series.mean()
-
-    # Cálculo do desvio percentual em relação à média anual
-    seasonal_index = (monthly_avg - annual_avg) / annual_avg * 100
-
-    # Criando o gráfico com Plotly
-    fig = go.Figure()
-    fig.add_trace(go.Bar(x=monthly_avg.index, y=seasonal_index.values, name='Índice de Sazonalidade (%)'))
-    fig.update_layout(title=title, xaxis_title='Mês', yaxis_title='Desvio Percentual da Média Anual (%)')
-    st.plotly_chart(fig)
-
 # Interface do Streamlit
-st.markdown("<h1 style='text-align: center;'>NEAD WebApp - Antecipação de Cenários Futuros para o 2G</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>NEAD WebApp - Antecipação de Cenários Futuros</h1>", unsafe_allow_html=True)
 st.write('Análise de séries temporais e predições baseadas em machine learning')
-st.write('Versão: 1.5')
-st.write('Data: 11/12/2024')
-st.write('Cientista de Dados: Álvaro Rösler')
-st.write('Supervisão: Sérgio Weber')
 
 # Seleção da variável a ser predita
 option = st.selectbox('Escolha a métrica para realizar a predição:', ('Acervo Líquido', 'Saldo de Entradas', 'Julgamentos'))
@@ -120,10 +87,10 @@ option = st.selectbox('Escolha a métrica para realizar a predição:', ('Acervo
 # Selecionar o número de meses para predição
 steps = st.slider('Número de meses para predição', 1, 12, 6)
 
-# Variável alvo
+# Definição de endógena e exógena
 if option == 'Saldo de Entradas':
     endog = df['Saldo de Entradas']
-    exog = df[['Sentenças_JE', 'Sentenças_1G']]
+    exog = df[['Julgamentos']]
 elif option == 'Julgamentos':
     endog = df['Julgamentos']
     exog = df[['Saldo de Entradas']]
@@ -134,7 +101,6 @@ else:
 # Ajustar o modelo e obter a previsão
 model_fit, forecast_values, forecast_index = sarimax_model(endog, exog, steps)
 
-
 # Decomposição da série temporal
 decomposition = decompose_series(endog)
 
@@ -142,30 +108,25 @@ decomposition = decompose_series(endog)
 st.subheader('Componente de Tendência')
 plot_decomposition(decomposition, 'Tendência')
 
-# Gráfico de Sazonalidade
-st.subheader('Componente de Sazonalidade')
-plot_seasonality(decomposition, 'Sazonalidade')
-
-# Gráfico do Índice de Sazonalidade Mensal (Desvio Percentual da Média Anual)
-st.subheader('Índice de Sazonalidade Mensal (Desvio Percentual da Média Anual)')
-plot_seasonal_index(endog, 'Índice de Sazonalidade Mensal (%)')
-
-# Formatar a coluna 'Data' para exibir o mês e ano no formato desejado (Outubro de 2024, etc.)
-results = pd.DataFrame({
-    'Mês e Ano': forecast_index.strftime('%B de %Y'),  # Agora os meses estão em português
-    'Valor Predito': forecast_values.apply(lambda x: f"{x:,.0f}".replace(",", ".")),
-    'Último Valor Observado Previamente': endog[-steps:].values,
-    'Variação (%)': (forecast_values - endog[-steps:].values) / endog[-steps:].values * 100
-})
-
 # Gráfico da série temporal com predições
 st.subheader(f'Série Temporal e Valores Preditores - {option}')
 plot_series(endog, forecast_values, forecast_index, f'Predição para {option}')
 
 # Mostrar AIC do modelo
-st.write(f"AIC do modelo de predição (SARIMAX): {model_fit.aic:.0f}")
+st.write(f"AIC do modelo de predição (SARIMAX): {model_fit.aic:.2f}")
 
-# Exibir a tabela com valores preditos, observados e variação percentual, ocultando a coluna extra de data
+# Últimos valores observados no mesmo mês do ano anterior
+last_year_values = endog.shift(12).reindex(forecast_index)
+
+# Resultados da predição
+results = pd.DataFrame({
+    'Mês e Ano': forecast_index.strftime('%B de %Y'),
+    'Valor Predito': forecast_values,
+    'Último Valor Observado Previamente': last_year_values.values,
+    'Variação (%)': (forecast_values - last_year_values.values) / last_year_values.values * 100
+})
+
+# Exibir a tabela com valores preditos
 st.write('Tabela de Predições:')
 st.dataframe(results)
 
